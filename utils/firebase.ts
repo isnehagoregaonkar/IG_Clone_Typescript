f691b00511adce93581ce74aa9551ba24cea6ff2
Snehal Goregaonkar <goregaonkarsneha98@gmail.com>
2023-07-18 13:39:56 +0530

    final commit

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -9,13 +9,7 @@ import {getAuth} from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBd8_vT_3ZzOtq39EYEOErvTh4YfRBVzx0",
-  authDomain: "ig-clone-typescript.firebaseapp.com",
-  projectId: "ig-clone-typescript",
-  storageBucket: "ig-clone-typescript.appspot.com",
-  messagingSenderId: "704226862222",
-  appId: "1:704226862222:web:0d1a6781895804cdb2d5df",
-  measurementId: "G-N43F802HQB"
+
 };
 
 // Initialize Firebase
